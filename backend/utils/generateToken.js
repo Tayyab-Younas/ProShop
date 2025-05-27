04bc67beeefdc46f304cfc54eb6c30842e3b7f6c
Tayyab-Younas <tabahis700@gmail.com>
2025-05-27 08:20:44 +0500

    update token life

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -10,7 +10,7 @@ const generateToken = (res, userId) => {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
     sameSite: "strict",
-    maxAge: 2 * 24 * 60 * 60 * 100, //2 Day
+    maxAge: 6* 24 * 60 * 60 * 100, //6 Day
   });
 };
 
